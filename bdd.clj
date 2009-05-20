f78981cd2964dc99c5d7ea31348f10e9f05d8b8e
Ed Bowler <ed.bowler@gmail.com>
2009-05-20 14:46:11 +0100

    defsteps working - needs work ... starting execute-scenario

--- bdd.clj
+++ bdd.clj
@@ -3,16 +3,75 @@
 
 (def steps (ref []))
 
-(defstruct step :stage)
+(defstruct step :stage :keywords :implemntation)
 
 (with-test
 	(defn- map-elements-to-strings [[ & elements]]
 	  (map (fn [a] (str a)) elements))
   (is (= ["a" "b" "c"] (map-elements-to-strings ['a 'b 'c]))))
 
-(defn defstep "Use this to create the steps that implement the scenarios" [[stage & elements] fn]
-  (dosync (ref-set steps (conj @steps (struct step stage)))))
+(with-test
+	(defn defstep "Use this to create the steps that implement the scenarios" [[stage & keywords] fn]
+	  (dosync (ref-set steps (conj @steps (struct step stage keywords fn)))))
+  (binding [steps (ref [])]
+	(let [impl (fn [] 1)]
+	  (defstep [:given "a" "b" "c"] impl)
+	  (is (= @steps [{:stage :given :keywords ["a" "b" "c"] :implemntation impl}])))))
 
+(with-test
+	(defn- match-step-keywords? [scenario-keywords step-keywords]
+	  (let [scenario-keyword (first scenario-keywords)
+			step-keyword (first step-keywords)
+			compare-as-strings (fn [sc st] (and (string? st) (. st equalsIgnoreCase sc)))]
+		(cond
+		  (and (= 0 (count scenario-keywords) (count step-keywords)))
+		  true
+
+		  (and (fn? step-keyword) (= 1 (count step-keywords))
+			   (= 0 (count scenario-keywords)))
+		  true
+
+		  (compare-as-strings scenario-keyword step-keyword)
+		  (recur (rest scenario-keywords) (rest step-keywords))
+		  
+		  (fn? step-keyword)
+		  (if (compare-as-strings (second scenario-keywords) (second step-keywords))
+			(recur (rest scenario-keywords) (rest step-keywords))
+			(recur (rest scenario-keywords) step-keywords))
+		  
+		  :else false)))
+
+  (is (= false (match-step-keywords? ["a"] ["a" "b"])))
+  (is (= true (match-step-keywords? ["a" "b" "c"] ["a" "b" "c"])))
+  (is (= true (match-step-keywords? ["aaa" "bbb" "ccc"] ["aaa" "bbb" "ccc"])))
+  (is (= false (match-step-keywords? ["a" "b" "Q"] ["a" "b" "c"])))
+  (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c d] 1) "e"])))
+  (is (= false (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c] 1) "d"])))
+  (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c d e] 1)]))))
+
+(defn- get-matching-steps [stage keywords]
+  (filter (fn [step]
+			(and (= stage (step :stage))
+				 (match-step-keywords? keywords (step :keywords))))
+		  steps))
+
+(with-test
+	(defn- match-steps? [stage & keywords]
+	  (let [matching-steps (get-matching-steps stage keywords)]
+		(if (= 1 (count matching-steps))
+		  ((first matching-steps):implemntation)
+		  false)))
+
+  (let [impl-1 (fn [] 1)
+		impl-2 (fn [] 2)
+		impl-3 (fn [] 3)]
+	(binding [steps [{:stage :given :keywords ["a" "b" "c"] :implemntation impl-1}
+					 {:stage :when :keywords ["a" "b" "c"] :implemntation impl-2}
+					 {:stage :then :keywords ["a" "b" "c"] :implemntation impl-3}]]
+	  (is (= impl-1 (match-steps? :given "a" "b" "c")))
+	  (is (= impl-2 (match-steps? :when "a" "b" "c")))
+	  (is (= impl-3 (match-steps? :then "a" "b" "c"))))))
+  
 (defn- tokenize-scenario 
   ([test-clauses]
 	 (tokenize-scenario nil [] test-clauses))
@@ -30,7 +89,7 @@
 			 (throw (new IllegalArgumentException (str "missing given - found " token)))
 			 (recur :given [:given] (vec (rest test-clauses)))))))))
 
-(defn group-tokens 
+(defn- group-tokens 
   ([tokens]
 	 (group-tokens [] tokens))
   ([accum tokens]
@@ -50,6 +109,10 @@
   (is (= [[:given "a"] [:when "b"] [:then "c"]]
 		 (parse-scenario "Given" "a" "When" "b" "Then" "c"))))
 
+(defn- execute-scenario [& test-clauses]
+  (doseq [test-clause test-clauses]
+	))
+  
 (defmacro scenario "The BDD scenario definition macro"
   [title & test-clauses]
   (conj (map-elements-to-strings test-clauses) `parse-scenario))
@@ -78,6 +141,7 @@
 (deftest test-strings-group-tokens
   (binding [steps (ref [])
 			test-fn-map (new java.util.HashMap)]
+
 	(defstep [:given 'a 'string (fn [t] (println t))]
 	  (fn [arg]
 		(. test-fn-map put :given arg)))
@@ -87,12 +151,13 @@
 	(defstep [:then 'there 'is (fn [n] n)]
 	  (fn [arg]
 		(. test-fn-map put :then arg)))
+
 	(scenario "strings are one token"
 			  Given a string "with some spaces"
 			  When the tokens are counted
 			  Then there is 1)
+
 	(is (= 3 (count @steps)))
 	(is (= "with some spaces" (. test-fn-map get :given)))
 	(is (= "when" (. test-fn-map get :when)))
 	(is (= "1" (. test-fn-map get :then)))))
-
