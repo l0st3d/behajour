dc7e594d6b809cbe11237e3f74985b2501e01611
Ed Bowler <ed.bowler@gmail.com>
2009-05-25 02:17:29 +0100

    working on it ...

--- bdd.clj
+++ bdd.clj
@@ -2,9 +2,12 @@
   (:use clojure.contrib.test-is))
 
 (import 'java.io.StringWriter)
+(import 'java.util.HashMap)
 
 (def *steps* (ref []))
 (defstruct step :stage :keywords :implementation)
+
+(def test-fn-map (new HashMap))
 
 (with-test
 	(defn- map-elements-to-strings [[ & elements]]
@@ -12,12 +15,38 @@
   (is (= ["a" "b" "c"] (map-elements-to-strings ['a 'b 'c]))))
 
 (with-test
-	(defn defstep "Use this to create the steps that implement the scenarios" [[stage & keywords] fn]
+	(defn define-step [stage keywords fn]
 	  (dosync (alter *steps* conj (struct step stage keywords fn))))
   (binding [*steps* (ref [])]
 	(let [impl (fn [] 1)]
-	  (defstep [:given "a" "b" "c"] impl)
+	  (define-step :given ["a" "b" "c"] impl)
 	  (is (= @*steps* [{:stage :given :keywords ["a" "b" "c"] :implementation impl}])))))
+
+(with-test
+	(defn- make-keyword [keywd]
+	  (let [k (. (str keywd) toLowerCase)]
+		(if (some #(= % k) ["given" "when" "then"])
+		  (keyword k)
+		  (throw (new IllegalArgumentException (str "unexpected keyword : " keyword))))))
+  (is (thrown? IllegalArgumentException (make-keyword 'asdf)))
+  (is (= :given (make-keyword 'Given)))
+  (is (= :when (make-keyword "when")))
+  (is (= :then (make-keyword 'THEN))))
+
+(defmacro defstep "Use this to create the steps that implement the scenarios"
+  [[stage & keywords] args & body]
+  `(define-step ~(make-keyword stage) ~(vec (map-elements-to-strings keywords))
+		(fn ~args ~@body)))
+
+(deftest test-defstep
+  (is (= `(define-step :given ["a" "thing"]
+			(fn [] (println "fish")))
+		 (macroexpand-1 
+		  '(defstep
+			 [Given a thing]
+			 []
+			 (clojure.core/println "fish")))))
+  (is (vector? (nth (macroexpand-1 '(defstep [given a thing] [] (println "fish"))) 2))))
 
 (with-test
 	(defn- match-step-keywords? [scenario-keywords step-keywords]
@@ -48,7 +77,9 @@
   (is (= false (match-step-keywords? ["a" "b" "Q"] ["a" "b" "c"])))
   (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c d] 1) "e"])))
   (is (= false (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c] 1) "d"])))
-  (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c d e] 1)]))))
+  (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" (fn [b c d e] 1)])))
+  (is (= true (match-step-keywords? ["a" "b" "c" "d" "e"] ["a" #(%) "c" #(%) "e"])))
+  (is (= true (match-step-keywords? ["a" "b" "c" "d"] ["a" #(%) "c" #(%)]))))
 
 (with-test
 	(defn- get-matching-steps [stage keywords]
@@ -87,7 +118,7 @@
 		   (if (. "and" equalsIgnoreCase token)
 			 (recur stage (conj accum stage) (vec (rest test-clauses)))
 			 (if (or (. "then" equals token) (. "when" equals token))
-			   (recur stage (conj accum (keyword token)) (vec (rest test-clauses)))
+			   (recur stage (conj accum (keyword (. token toLowerCase))) (vec (rest test-clauses)))
 			   (recur stage (conj accum token) (vec (rest test-clauses)))))
 		   (if (not (= token "given"))
 			 (throw (new IllegalArgumentException (str "missing given - found " token)))
@@ -116,8 +147,7 @@
 (defn- print-step [stage test-clauses]
   (print (str stage " "))
   (doseq [clause test-clauses]
-	(print (str clause " ")))
-  (println "(PENDING)"))
+	(print (str clause " "))))
 
 (with-test
 	(defn- get-test-fn-args 
@@ -138,65 +168,111 @@
   (is (= ["b" "c" "d"] (get-test-fn-args ["a" "b" "c" "d" "e"] ["a" (fn [b c d] [b c d]) "e"])))
   (is (= ["b"] (get-test-fn-args ["a" "b" "c"] ["a" (fn [a] a) "c"]))))
 
-(defn- execute-scenario [tests]
-  (doseq [test-line tests]
-	(let [[stage & test-clauses] test-line
-		  step (match-steps? stage test-clauses)]
-	  (with-test-out
-		(if step
-		  (apply (step :implementation) (get-test-fn-args test-clauses (step :keywords)))
-		  (print-step stage test-clauses))))))
+(defn- execute-scenario 
+  ([title tests]
+	 (binding [*out* (new StringWriter)]
+	   (execute-scenario title tests false)
+	   (println "\n\n")))
+  ([title tests test-known-pending]
+	 (println (str "\n\n Scenario : " title "\n"))
+	 (let [test-line (first tests)
+		   [stage & test-clauses] test-line
+			 step (match-steps? stage test-clauses)]
+		 (print-step stage test-clauses)
+		 (if (and step (not test-known-pending))
+		   (apply (step :implementation) (get-test-fn-args test-clauses (step :keywords)))
+		   (println "(PENDING)"))
+		 (comment step
+		   (recur title (rest tests) test-known-pending)
+		   (recur title (rest tests) true)))))
   
 (defmacro scenario "The BDD scenario definition macro"
   [title & test-clauses]
-  (list `execute-scenario (conj (map-elements-to-strings test-clauses) `parse-scenario)))
+  `(execute-scenario ~title (parse-scenario ~@(map-elements-to-strings test-clauses))))
 
 (deftest integration-test
-  (scenario "the bdd library runs tests in a given, when, then format"
-			given a precondition with some data in
-			and another precondition
-			When an action happens
-			and another action happens that requires some input
-			Then a result is true
-			and another result is equal to some data
-			and a final test that we received some input))
+  (binding [*steps* (ref [])
+			test-fn-map (new HashMap)]
+	(defstep
+		[given a precondition with some data in]
+		[]
+	  (println "a"))
+
+
+	(scenario "the bdd library runs tests in a given, when, then format"
+			  given a precondition with some data in
+			  and another precondition
+			  When an action happens
+			  and another action happens that requires some input
+			  Then a result is true
+			  and another result is equal to some data
+			  and a final test that we received some input)))
 
 (deftest test-multiple-actions
-  (scenario "the + and * functions"
-			Given a number 1
-			and a number 2
-			When the numbers are summed
-			Then a result is 3
-			When the numbers are multiplied
-			Then a result is 2))
+  (binding [*steps* (ref [])
+			test-fn-map (new HashMap)]
+
+	(defstep
+		[given a #(%) number #(num %)]
+		[k n]
+	  (. test-fn-map put k n))
+
+	(defstep
+		[When the numbers are summed]
+		[]
+	  (. test-fn-map put "answer" (+ (. test-fn-map get "first")
+									 (. test-fn-map get "second"))))
+
+	(defstep
+		[When the numbers are multiplied]
+		[]
+	  (. test-fn-map put "answer" (* (. test-fn-map get "first")
+								  (. test-fn-map get "second"))))
+
+	(defstep
+		[Then a result is (fn [n] (num n))]
+		[n]
+	  (is (= n (. test-fn-map get "answer"))))
+	  
+	(scenario "the + and * functions"
+			  Given a first number 1
+			  and a second number 2
+			  When the numbers are summed
+			  Then a result is 3
+			  When the numbers are multiplied
+			  Then a result is 2))
+	)
 
 (deftest test-steps-are-displayed-as-pending
   (binding [*test-out* (new StringWriter)]
 	(scenario "test pending" Given something)
 	(is (. (str *test-out*) matches ".*PENDING.*"))))
 
-(def test-fn-map (new java.util.HashMap))
-
 (deftest test-strings-group-tokens
   (binding [*steps* (ref [])
-			test-fn-map (new java.util.HashMap)]
+			test-fn-map (new HashMap)]
 
-	(defstep [:given "a" "string" (fn [t] (println t))]
-	  (fn [arg]
-		(. test-fn-map put :given arg)))
-	(defstep [:when "the" "tokens" "are" "counted"]
-	  (fn []
-		(. test-fn-map put :when "when")))
-	(defstep [:then "there" "is" (fn [n] n)]
-	  (fn [arg]
-		(. test-fn-map put :then arg)))
+	(defstep
+		[given a string (fn [t] (. test-fn-map put "first conversion string called" "true") t)]
+		[arg]
+	  (. test-fn-map put :given arg))
+	(defstep 
+		[when the tokens are counted]
+		[]
+	  (. test-fn-map put :when "when"))
+	(defstep
+		[then there is (fn [n] (. test-fn-map put "second conversion string called" "true") n) token]
+		[arg]
+	  (. test-fn-map put :then arg))
 
 	(scenario "strings are one token"
 			  Given a string "with some spaces"
 			  When the tokens are counted
-			  Then there is 1)
+			  Then there is 1 token)
 
 	(is (= 3 (count @*steps*)))
 	(is (= "with some spaces" (. test-fn-map get :given)))
 	(is (= "when" (. test-fn-map get :when)))
-	(is (= "1" (. test-fn-map get :then)))))
+	(is (= "1" (. test-fn-map get :then)))
+	(is (= "true" (. test-fn-map get "first conversion string called")))
+	(is (= "true" (. test-fn-map get "second conversion string called")))))
